3ddde4f368307b8a7f9067af612b14acbed9326b
AnandhakumarG <anandhakumarsrg@outlook.com>
2023-12-27 16:41:00 +0530

    Revert "Second Commit"
    This reverts commit 7b5653fb42f5e4fa89c3e1042aff727bf1eaf3d7.

--- src/test/java/gittest/test.java
+++ src/test/java/gittest/test.java
@@ -6,15 +6,12 @@ public class test {
 		System.out.println(" Hello world");
 	}
 	
-	public void greetings() {
-		System.out.println(" Hello world");
-	}
 	
 	
 	
 	public static void main(String[] args){
         test obj=new test();
 	obj.greeting();
-	obj.greetings();
+
 }
 }
