532f15647d2d047bc3167795a09e586b375ae562
AnandhakumarG <anandhakumarsrg@outlook.com>
2023-12-27 16:58:29 +0530

    Dev Commit

--- src/test/java/gittest/test.java
+++ src/test/java/gittest/test.java
@@ -6,12 +6,16 @@ public class test {
 		System.out.println(" Hello world");
 	}
 	
+	public void test() {
+		System.out.println(" Hello world");
+	}
 	
 	
 	
 	public static void main(String[] args){
         test obj=new test();
 	obj.greeting();
+	obj.test();
 
 }
 }
