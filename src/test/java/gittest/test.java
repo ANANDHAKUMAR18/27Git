7b5653fb42f5e4fa89c3e1042aff727bf1eaf3d7
AnandhakumarG <anandhakumarsrg@outlook.com>
2023-12-27 16:40:16 +0530

    Second Commit

--- src/test/java/gittest/test.java
+++ src/test/java/gittest/test.java
@@ -6,12 +6,15 @@ public class test {
 		System.out.println(" Hello world");
 	}
 	
+	public void greetings() {
+		System.out.println(" Hello world");
+	}
 	
 	
 	
 	public static void main(String[] args){
         test obj=new test();
 	obj.greeting();
-
+	obj.greetings();
 }
 }
