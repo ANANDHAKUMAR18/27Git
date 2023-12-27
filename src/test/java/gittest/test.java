7aa23c10c0625f724ba79f3484eaf877e8791e80
AnandhakumarG <anandhakumarsrg@outlook.com>
2023-12-27 16:31:40 +0530

    First Commit

---
+++ src/test/java/gittest/test.java
@@ -0,0 +1,17 @@
+package gittest;
+
+public class test {
+	
+	public void greeting() {
+		System.out.println(" Hello world");
+	}
+	
+	
+	
+	
+	public static void main(String[] args){
+        test obj=new test();
+	obj.greeting();
+
+}
+}
